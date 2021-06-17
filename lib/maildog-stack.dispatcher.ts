f2f0e54433cd60e3854f3bf8b85f689a2cd1c098
Edmund Hung <me@edmund.dev>
2021-06-17 18:57:16 +0200

    feat: use sns to initiate dispatcher lambda function

--- lib/maildog-stack.dispatcher.ts
+++ lib/maildog-stack.dispatcher.ts
@@ -1,7 +1,7 @@
-import { SESHandler } from 'aws-lambda';
+import { SNSHandler, SESEventRecord } from 'aws-lambda';
 import LambdaForwarder from 'aws-lambda-ses-forwarder';
 
-export const handler: SESHandler = (event, context, callback) => {
+export const handler: SNSHandler = (event, context, callback) => {
   const config = process.env.CONFIG ?? {};
   const overrides = {
     config: {
@@ -10,5 +10,31 @@ export const handler: SESHandler = (event, context, callback) => {
     },
   };
 
-  LambdaForwarder.handler(event, context, callback, overrides);
+  // Simulate SES Event so we can utilise aws-lambda-ses-forwarder for now
+  // Based on documentation from
+  // https://docs.aws.amazon.com/ses/latest/DeveloperGuide/receiving-email-notifications-contents.html#receiving-email-notifications-contents-top-level-json-object
+  const sesEvent = {
+    Records: event.Records.map<SESEventRecord>((r) => {
+      if (r.EventSource !== 'aws:sns') {
+        callback(
+          new Error(
+            'Invalid event received; Only SNS Event is expected at the moment',
+          ),
+        );
+      }
+
+      const { mail, receipt } = JSON.parse(r.Sns.Message);
+
+      return {
+        eventSource: 'aws:ses',
+        eventVersion: '1.0',
+        ses: {
+          mail,
+          receipt,
+        },
+      };
+    }),
+  };
+
+  LambdaForwarder.handler(sesEvent, context, callback, overrides);
 };
