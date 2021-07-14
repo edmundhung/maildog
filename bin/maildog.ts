9694f829d4fc30ced0a82291f9665d2ae57975bd
Edmund Hung <me@edmund.dev>
2021-07-14 22:11:34 +0200

    feat: add support of comment on the JSON config file with `jsonc`
    fix #7

--- bin/maildog.ts
+++ bin/maildog.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import * as fs from 'fs';
+import { parse } from 'jsonc-parser';
 import { MailDogStack } from '../lib/maildog-stack';
-import config from '../maildog.config.json';
 
 const app = new cdk.App();
+const config = parse(fs.readFileSync('./maildog.config.json').toString());
 
 new MailDogStack(app, 'MailDog', {
   config,
