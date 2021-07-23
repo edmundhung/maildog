566b23c7a515d33848cf7b51ad7099ef7ce5cc56
Edmund Hung <me@edmund.dev>
2021-07-24 00:16:14 +0200

    feat(web): implement github oauth web application flow with installation support

--- packages/web/app/routes/index.tsx
+++ packages/web/app/routes/index.tsx
@@ -1,7 +1,39 @@
 import type { LoaderFunction } from 'remix';
-import { redirect } from 'remix';
+import { useRouteData, redirect, json } from 'remix';
+import { listInstalledRepositories } from '../github';
+import { getSession } from '../auth';
 
-export default function Index() {
+export let loader: LoaderFunction = async ({ request }) => {
+  const session = await getSession(request);
+
+  if (!session.has('accessToken')) {
+    return json({
+      isAuthenticated: false,
+      repos: [],
+    });
+  }
+
+  const token = session.get('accessToken');
+  const repos = await listInstalledRepositories(token);
+
+  if (repos.length === 0) {
+    console.log(
+      'No installed repository found; Redirecting user to Github App installation page',
+    );
+    return redirect(
+      'https://github.com/apps/maildog-trainer/installations/new',
+    );
+  }
+
+  return json({
+    isAuthenticated: true,
+    repos,
+  });
+};
+
+export default function Index(): React.ReactElement {
+  const { isAuthenticated, repos } = useRouteData();
+
   return (
     <div>
       <h1>Bark! 🐶</h1>
@@ -9,14 +41,28 @@ export default function Index() {
         Your{' '}
         <a
           className="border-dotted border-b border-black dark:border-white"
-          href="https://github.com/edmundhung/maildog"
+          href={`https://github.com/${repos[0] ?? 'edmundhung/maildog'}`}
         >
-          maildog
+          {repos[0] ?? 'maildog'}
         </a>{' '}
         is waiting for you
       </div>
       <footer className="pt-12 text-xs">
-        It's still a puppy. But we are training it for you...
+        {isAuthenticated ? (
+          <a
+            className="border-dotted border-b border-black dark:border-white"
+            href="/logout"
+          >
+            Logout
+          </a>
+        ) : (
+          <a
+            className="border-dotted border-b border-black dark:border-white"
+            href="/login"
+          >
+            Login
+          </a>
+        )}
       </footer>
     </div>
   );
