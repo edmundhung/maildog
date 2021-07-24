7d08c48e4f180484b6511abb63135bdaa21157f0
Edmund Hung <me@edmund.dev>
2021-07-24 23:34:01 +0200

    feat(extensions): setup tailwindcss

---
+++ packages/extensions/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./public/*.html', './src/**/*.ts', './src/**/*.tsx'],
+  darkMode: 'media',
+  theme: {},
+  variants: {
+    extend: {
+      borderColor: ['dark'],
+    },
+  },
+  plugins: [],
+};
