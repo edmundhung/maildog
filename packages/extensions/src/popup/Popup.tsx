e06053515ba9961ed77a58f7fee0d65999a0cbb4
Edmund Hung <me@edmund.dev>
2021-08-09 22:18:11 +0200

    refactor(extensions): rewrite state management using xstate

--- packages/extensions/src/popup/Popup.tsx
+++ packages/extensions/src/popup/Popup.tsx
@@ -7,30 +7,6 @@ import MainScreen from './MainScreen';
 import * as api from './api';
 import { Status } from '../types';
 
-async function openPageInBackground(path: string): Promise<Tabs.Tab> {
-  const backgroundTab = await browser.tabs.create({
-    url: `${process.env.WEB_URL ?? 'http://localhost:3000'}${path}`,
-    active: false,
-  });
-
-  return new Promise((resolve) => {
-    function listener(
-      tabId: number,
-      changeInfo: Tabs.OnUpdatedChangeInfoType,
-      tab: Tabs.Tab,
-    ): void {
-      if (tabId !== backgroundTab.id || changeInfo.status !== 'complete') {
-        return;
-      }
-
-      resolve(tab);
-      browser.tabs.onUpdated.removeListener(listener);
-    }
-
-    browser.tabs.onUpdated.addListener(listener);
-  });
-}
-
 async function showInstallationPage(): Promise<void> {
   await browser.tabs.create({
     url: 'https://github.com/apps/maildog-bot/installations/new',
@@ -42,92 +18,54 @@ async function showOfficialWebsite(): Promise<void> {
 }
 
 function Popup(): React.ReactElement {
-  const [isRefreshing, setRefreshing] = useState(false);
+  const [repository, setRepository] = useState<string | null>(null);
   const [shouldShowMenu, setShowMenu] = useState(false);
   const queryClient = useQueryClient();
   const { data, isFetched, refetch } = useQuery({
-    queryKey: 'GET_STATUS',
-    queryFn: api.getStatus,
-    onSettled() {
-      setRefreshing(false);
-    },
-  });
-  const selectRepository = useMutation({
-    mutationFn: api.selectRepository,
-    async onMutate(repository) {
-      // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
-      await queryClient.cancelQueries('GET_STATUS');
+    queryKey: 'GET_SESSION',
+    queryFn: api.getSession,
+    onSuccess(session) {
+      if (session === null) {
+        return;
+      }
 
-      // Snapshot the previous value
-      const previousStatus = queryClient.getQueryData('GET_STATUS');
-
-      // Optimistically update to the new value
-      queryClient.setQueryData('GET_STATUS', (status: Status) => {
-        if (repository === status.repository) {
-          return status;
+      setRepository((current) => {
+        if (current !== null) {
+          return current;
         }
 
-        return {
-          ...status,
-          repository,
-          configByDomain: null,
-          emails: [],
-        };
+        return session.repository ?? session.options[0] ?? null;
       });
-
-      // Return a context object with the snapshotted value
-      return { previousStatus };
-    },
-    // Always refetch after error or success:
-    onSettled() {
-      queryClient.invalidateQueries('GET_STATUS');
-    },
-    onSuccess() {
-      setShowMenu(false);
-    },
-    onError(err, newTodo, context) {
-      // If the mutation fails, use the context returned from onMutate to roll back
-      queryClient.setQueryData('GET_STATUS', context.previousStatus);
     },
   });
-  const savePassphrase = useMutation({
-    mutationFn: api.savePassphase,
+  const unlock = useMutation({
+    mutationFn: (passphrase: string) => api.unlock(repository, passphrase),
     onSuccess() {
-      queryClient.invalidateQueries('GET_STATUS');
+      queryClient.invalidateQueries('GET_SESSION');
     },
   });
-
-  const login = async () => {
-    setRefreshing(true);
-    const tab = await openPageInBackground('/login');
-
-    if (!tab.url) {
-      browser.tabs.update(tab.id, { active: true });
-      return;
-    }
-
-    browser.tabs.remove(tab.id);
-    refetch();
-  };
-
-  const logout = async () => {
-    setRefreshing(true);
-    const tab = await openPageInBackground('/logout');
-
-    browser.tabs.remove(tab.id);
-    await api.reset();
-    setShowMenu(false);
-    refetch();
-  };
+  const login = useMutation({
+    mutationFn: api.login,
+    onSuccess() {
+      queryClient.invalidateQueries('GET_SESSION');
+    },
+  });
+  const logout = useMutation({
+    mutationFn: api.logout,
+    onSuccess() {
+      queryClient.invalidateQueries('GET_SESSION');
+    },
+  });
 
   const shouldAuthenitcate = isFetched && data === null;
-  const isLoading = !isFetched || isRefreshing || savePassphrase.isLoading;
+  const isLoading =
+    !isFetched || login.isLoading || logout.isLoading || unlock.isLoading;
 
   if (isLoading || shouldAuthenitcate) {
     return (
       <LoginScreen
         isLoading={isLoading}
-        onLogin={login}
+        onLogin={login.mutate}
         onLearnMore={showOfficialWebsite}
       />
     );
@@ -138,21 +76,26 @@ function Popup(): React.ReactElement {
       <NavigationScreen
         repository={data?.repository ?? null}
         options={data?.options ?? []}
-        onSelect={selectRepository.mutate}
+        onSelect={(repository) => {
+          setRepository(repository);
+          setShowMenu(false);
+        }}
         onInstall={showInstallationPage}
-        onLogout={logout}
+        onLogout={logout.mutate}
       />
     );
   }
 
   return (
     <MainScreen
-      repository={data?.repository ?? ''}
-      configByDomain={data?.configByDomain ?? null}
-      emails={data?.emails ?? []}
+      repository={repository}
+      configByDomain={
+        repository === data?.repository ? data?.configByDomain ?? null : null
+      }
+      emails={repository === data?.repository ? data?.emails ?? [] : []}
       onNavigate={() => setShowMenu(true)}
       onNewEmailRequested={() => {}}
-      onPassphraseProvided={savePassphrase.mutate}
+      onPassphraseProvided={unlock.mutate}
       onOptionUpdate={() => {}}
     />
   );
